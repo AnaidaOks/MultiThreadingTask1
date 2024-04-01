44021976f8a069c8bf6146e32308e44322d166b9
Anaida Oksuzyan <aoa2190@gmail.com>
2024-04-01 19:37:34 +0300

    4 commit

--- src/main/java/myThread/Main.java
+++ src/main/java/myThread/Main.java
@@ -1,59 +1,42 @@
 package myThread;
 
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
         long startTs = System.currentTimeMillis(); // start time
-
-        final ExecutorService threadPool = Executors.newFixedThreadPool(5);
-
-        List<Future> lFuture = new ArrayList<>();
-
+        List<Thread> lThread = new ArrayList<>();
         for (String text : texts) {
-            final Future<Integer> task = threadPool.submit(new Callable<Integer>() {
-                @Override
-                public Integer call() throws Exception {
-                    int maxSize = 0;
-                    for (int i = 0; i < text.length(); i++) {
-                        for (int j = 0; j < text.length(); j++) {
-                            if (i >= j) {
-                                continue;
-                            }
-                            boolean bFound = false;
-                            for (int k = i; k < j; k++) {
-                                if (text.charAt(k) == 'b') {
-                                    bFound = true;
-                                    break;
-                                }
-                            }
-                            if (!bFound && maxSize < j - i) {
-                                maxSize = j - i;
+            Thread thread = new Thread(() -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
                             }
                         }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
                     }
-                    return maxSize;
                 }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
             });
-            lFuture.add(task);
+            lThread.add(thread);
+            thread.start();
         }
-
-        for (Future ft : lFuture) {
-
-            System.out.println("maxSize: " + ft.get());
-        }
-
         long endTs = System.currentTimeMillis(); // end time
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
